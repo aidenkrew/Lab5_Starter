832f8510c867d9cb8ee0e37d7d04330f291f6cbb
Aiden Krueger <ajkrueger@ucsd.edu>
2025-05-05 17:50:46 -0700

    testing branch: should be wrong

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Lab 5 - Starter
 Make sure you make a PR to your own repo's main and not the class' repo!! Otherwise you will lose points!!
 Aiden Krueger
+https://aidenkrew.github.io/Lab5_Starter/expose.html
+
+1) Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+
+No since the the actual impplementation of message has more than 1 part so it would be hard to unit test it since it takes more than one small seciton of code.
+
+2) Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+
+Yes since we are just testing one part of the message which is the max message length, that is something that we should be able to unit test.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -8,4 +8,123 @@ import {
   isHexColor,
 } from '../code-to-unit-test/unit-test-me';
 
+//PHone number
+test('Phone Number with 10 digits should pass', () =>{
+  expect(isPhoneNumber('123-456-7890')).toBe(true);
+})
+
+test('Phone Number not valid should not pass', () =>{
+  expect(isPhoneNumber('3904578452389')).toBe(false);
+})
+
+test('Valid phone number', () => {
+  expect(isPhoneNumber('456-7890')).toBe(true);
+});
+
+test('Phone Number not valid should not pass', () => {
+  expect(isPhoneNumber('(1233)-456234578905324')).toBe(false);
+});
+
+//Email
+test('Valid email should pass', () => {
+expect(isEmail('ak@gmail.com')).toBe(true)
+
+})
+
+
+test('Valid email should pass', () => {
+  expect(isEmail('l@gmail.com')).toBe(true)
+
+})
+
+
+test('inValid email should not pass', () => {
+  expect(isEmail('l@l@l.com')).toBe(false)
+
+})
+
+
+test('inValid email should not pass', () => {
+  expect(isEmail('pqoweiruoqiwrup')).toBe(false)
+
+})
+
+//Strong password
+
+test('Valid strong password, should pass', () => {
+  expect(isStrongPassword('wqruiop')).toBe(true)
+
+})
+
+
+test('Valid strong password, should pass', () => {
+  expect(isStrongPassword('pqiouw4e')).toBe(true)
+
+})
+
+
+test('invalid strong password, should not pass', () => {
+  expect(isStrongPassword('4weopiur')).toBe(false)
+
+})
+
+
+test('invalid strong password, should not pass', () => {
+  expect(isStrongPassword('qwerpoiu^')).toBe(false)
+
+})
+
+//Date
+
+test('Valid Date, should pass', () => {
+  expect(isDate('1/2/2025')).toBe(true)
+
+})
+
+
+test('Valid Date, should pass', () => {
+  expect(isDate('1/1/2004')).toBe(true)
+
+})
+
+
+test('inValid Date, should not pass', () => {
+
+  expect(isDate('143/654/65')).toBe(false)
+})
+
+
+test('invalid Date, should not pass', () => {
+  expect(isDate('1/1/1')).toBe(false)
+
+})
+
+//Hex Color
+
+test('valid Hex color, should pass', () => {
+  expect(isHexColor('#42f5e0')).toBe(true)
+
+})
+
+test('valid Hex color, should pass', () => {
+  expect(isHexColor('#4287f5')).toBe(true)
+
+})
+
+
+test('invalid Hex color, should not pass', () => {
+  expect(isHexColor('#qpriotu')).toBe(false)
+
+})
+
+
+test('invalid Hex color, should not pass', () => {
+  expect(isHexColor('')).toBe(false)
+
+})
+
+
+
+
+
 // TODO - Part 2
